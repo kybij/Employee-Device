be3e4602cc0515e5d308536dc49cb5794139e954
Kylee Bijerring <kylee24b@gmail.com>
2021-09-14 18:51:49 -0600

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql2")
+require("console.table")
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,19 +9,129 @@ const connection = mysql.createConnection({
     database: "tracker"
 })
 
-connection.connection(function(error){
-    if (error){
+connection.connect(function (error) {
+    if (error) {
         console.log(error)
     }
     displayMenu()
 })
 // , , , , , , and 
-function displayMenu () {
-   inquirer.prompt([{
-       type: "list",
-       message: "Choose the Following",
-       choices: ["view all departments", "view all roles","view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
-       name: "selection"
-  
+function displayMenu() {
+    inquirer.prompt([{
+        type: "list",
+        message: "Choose the Following",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
+        name: "selection"
+
     }])
+        .then(response => {
+            if (response.selection === "view all departments") {
+                viewDepartments()
+            }
+            if (response.selection === "view all roles") {
+                viewRoles()
+            }
+            if (response.selection === "view all employees") {
+                viewEmployees()
+            }
+            if (response.selection === "add a department") {
+                addDepartment()
+            }
+            if (response.selection === "add a role") {
+                addRole()
+            }
+            if (response.selection === "add an employee") {
+                addEmployee()
+            }
+            if (response.selection === "update an employee") {
+                updateEmployee()
+            }
+        })
+}
+
+function viewDepartments() {
+    connection.query("select * from department", function (error, data) {
+        console.table(data)
+        displayMenu()
+    })
+}
+
+function viewRoles() {
+    connection.query("select * from role", function (error, data) {
+        console.table(data)
+        displayMenu()
+    })
+}
+
+function viewEmployees() {
+    connection.query("select * from employee", function (error, data) {
+        console.table(data)
+        displayMenu()
+    })
+}
+
+function addDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the new department name",
+        name: "departmentName"
+    }])
+        .then(response => {
+            connection.query("insert into department(name) values(?)", response.departmentName, function (error, data) {
+                console.log("new department has been added")
+                displayMenu()
+            })
+        })
+}
+
+function addRole() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the new role name",
+        name: "newRole"
+    },
+    {
+        type: "input",
+        message: "What is the salary",
+        name: "addSalary"
+    },
+    {
+        type: "input",
+        message: "What is the department id",
+        name: "addId"
+    }
+    ]).then(response => {
+        connection.query("insert into role(title,salary,department_id) values(?,?,?)", [response.newRole, response.addSalary, response.addId], function (error, data) {
+            console.log("new role added")
+            displayMenu()
+        })
+    })
+}
+
+
+function addEmployee() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the employee first name",
+        name: "firstName"
+    },
+    {
+        type: "input",
+        message: "What is the employee last name",
+        name: "lastName"
+    },
+    {
+        type: "input",
+        message: "what is the role id",
+        name: "roleId"
+    }]).then(response => {
+        connection.query("insert into employee(first_name,last_name,role_id)values (?,?,?)", [response.firstName, response.lastName, response.roleId], function (error, data) {
+            console.log("new employee added")
+            displayMenu()
+        })
+    })
+}
+
+function updateEmployee() {
+
 }
